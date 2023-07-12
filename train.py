0e4c8e652503117c9a1673d9122e056c53658740
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-07-12 16:48:02 +0800

    lr=0.0015

--- train.py
+++ train.py
@@ -49,12 +49,12 @@ def train_step(params: dict, opt_state: Any, total_loss: Array, data_batch: Trai
 def main() -> None:
     global forward, optimize
 
-    lr = 0.0013
-    batch_size = 64
+    lr = 0.0015
+    batch_size = 15
     max_len_enc = 512
     max_len_dec = 64
     n_epochs = 8
-    rank = 0
+    rank = 1
     seed = 3407
 
     initialise_tpu('v4-16', n_devices=1, rank=rank)
